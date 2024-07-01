5cafc889aa0a2ed6539ddafd3e86a425be1d321e
sohan-bhat <64918912+sohan-bhat@users.noreply.github.com>
2024-06-30 20:28:30 -0700

    Made sure to get aiTools from correct directory

--- src/Tools/userInputs.js
+++ src/Tools/userInputs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getGroqChatCompletion } from '../../Tools/aiTools';
+import { getGroqChatCompletion } from '../Tools/aiTools';
 
 export class UserInputField extends Component {
 
