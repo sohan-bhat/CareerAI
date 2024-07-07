b518aa22550c215f385c45bd2f73feb84a9ff4b0
Sohan Bhat <sohanrambhat@gmail.com>
2024-07-07 12:41:39 -0500

    Attempted to add Google Analytics; Changed website tab logo and title

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import HomePage from './Pages/Home/HomePage';
 import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
+import ReactGA from 'react-ga4'
+ReactGA.initialize('G-YTJ4RF70GN')
+ReactGA.send({ hitType: "pageview", page: "/", title: "Home Page" })
 
-const Routes = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path='/' Component={HomePage} />
-            </Switch>
-        </Router>
+class Routes extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path='/' Component={HomePage} />
+                </Switch>
+            </Router>
     );
+    }
 };
 
 export default Routes;
