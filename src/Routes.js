40a9c38d944499839306f452b9ad6b51dbefc071
Sohan Bhat <sohanrambhat@gmail.com>
2024-07-07 15:09:20 -0500

    Tried fixing google tag

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import HomePage from './Pages/Home/HomePage';
 import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
-import ReactGA from 'react-ga4'
-ReactGA.initialize('G-YTJ4RF70GN')
-ReactGA.send({ hitType: "pageview", page: "/", title: "Home Page" })
+// import ReactGA from 'react-ga4'
+// ReactGA.initialize('G-YTJ4RF70GN')
+// ReactGA.send({ hitType: "pageview", page: "/", title: "Home Page" })
 
 class Routes extends Component {
     render() {
         return (
+            <>
             <Router>
                 <Switch>
                     <Route path='/' Component={HomePage} />
                 </Switch>
             </Router>
-    );
+            </>
+        );
     }
 };
 
